66e86c286145d9e5072ad0541905228c4b8954bd
simtor <simone.ram@ada.ac.uk>
2020-06-16 11:18:33 +0100

    first commit

---
+++ Random-selector.js
@@ -0,0 +1,31 @@
+// console.log('This is a random game')
+
+// console.log('=============================')
+
+
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// readline.question('Input a number: ', number  => {
+//   console.log(`Hey there ${number}!`);
+//   readline.close();
+// });
+
+
+
+// function getrandom(max){
+//     return Math.floor(Math.random() * Math.floor(max));
+// }
+
+
+// // function randomtwo(){
+// //     return Math.random();
+// // }
+
+// // console.log(randomtwo(50));
+// console.log(getrandom(100));
+// // console.log(getrandom(100));
+
