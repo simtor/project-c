43abdfeb71e9e31c053d175c6152ea97fc823a92
simtor <simone.ram@ada.ac.uk>
2020-06-16 11:23:35 +0100

    implemented for loop

--- Random-selector.js
+++ Random-selector.js
@@ -1,7 +1,45 @@
+const readline = require("readline");
+ 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+ 
+const minimumNumber = 1;
+ 
+rl.question(
+  "Enter the maximum number of the randomly generated value: ",
+  (answer) => {
+    var i;
+    for (i = 0; i < answer; i++){
+      // const minimumNumber = 1;
+    function getNumberOne(min = 1, max = 100){
+      var x = Math.random() * (max - min) + min;
+      return (x);
+    // console.log(x);
+    }
+
+    function getNumberTwo(min = 1, max = 100){
+      var y = Math.random() * (max - min) + min;
+      return (y);
+    // console.log(x);
+    }
+
+    var z = (Math.floor(getNumberOne(this.x)));
+    console.log(z)
+    var s = (Math.floor(getNumberTwo(this.y)));
+    console.log(s)
+
+    var r = (z + s);
+    console.log(r);
+
+    }
+  }
+);
+
 // console.log('This is a random game')
 
 // console.log('=============================')
-
 
 
 // const readline = require('readline').createInterface({
