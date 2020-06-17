c7764a9c56b7ad9923415a77a5b56d0a25c6b268
simtor <simone.ram@ada.ac.uk>
2020-06-17 15:02:17 +0100

    Moved code above

--- calc-tor.js
+++ calc-tor.js
@@ -1,9 +1,71 @@
-// console.log('This is a random game')
+const readline = require("readline");
+ 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+ 
+const minimumNumber = 1;
+ 
+function getNumberOne(min = 1, max = 100) {
+  var x = Math.random() * (max - min) + min;
+  return x;
+}
+ 
+function getNumberTwo(min = 1, max = 100) {
+  var y = Math.random() * (max - min) + min;
+  return y;
+}
 
-// console.log('=============================')
+let correctAswers = [];
+// var sum = correctAswers.reduce(function(a, b){
+//     return a + b;
+// }, 0);
+ 
+// console.log(sum); // Prints: 15
 
 
+function askForAnswer(z, s) {
+  return new Promise((resolve) => {
+    rl.question("Plese input your answers: ", (input) => {
+      var r = z + s;
+      if (r == input) {
+        console.log("Thats right");
+        correctAswers.push('Right')
+        // let total = correctAswers.push('Right')
+        // console.log("You got: ",total);
+        // console.count(correctAswers);
+      } else {
+        console.log("Thats wrong");
+      }
+      resolve();
+    //   console.count(correctAswers);
+    //  console.log(correctAswers);
+    //console.log(correctAswers.length);
+    });
+  });
+}
 
+var output = "";
+
+console.time("concatenation");
+
+for(var p = 1; p<1e6; p++){
+    output +=p;
+}
+
+rl.question("Enter the number of times you want to play: ", async (answer) => {
+  for (let i = 0; i < answer; i++) {
+    var z = Math.floor(getNumberOne(this.x));
+    console.log(z);
+    var s = Math.floor(getNumberTwo(this.y));
+    console.log(s);
+    await askForAnswer(z, s);
+  }
+  rl.close();
+  console.log("You got: ",correctAswers.length, "/", answer, "right");
+  console.timeEnd("concatenation");
+});
 // const readline = require('readline').createInterface({
 //   input: process.stdin,
 //   output: process.stdout
@@ -79,74 +141,7 @@
 // );
 
 
-const readline = require("readline");
- 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
- 
-const minimumNumber = 1;
- 
-function getNumberOne(min = 1, max = 100) {
-  var x = Math.random() * (max - min) + min;
-  return x;
-}
- 
-function getNumberTwo(min = 1, max = 100) {
-  var y = Math.random() * (max - min) + min;
-  return y;
-}
 
-let correctAswers = [];
-// var sum = correctAswers.reduce(function(a, b){
-//     return a + b;
-// }, 0);
- 
-// console.log(sum); // Prints: 15
-
-
-function askForAnswer(z, s) {
-  return new Promise((resolve) => {
-    rl.question("Plese input your answers: ", (input) => {
-      var r = z + s;
-      if (r == input) {
-        console.log("Thats right");
-        correctAswers.push('Right')
-        // let total = correctAswers.push('Right')
-        // console.log("You got: ",total);
-        // console.count(correctAswers);
-      } else {
-        console.log("Thats wrong");
-      }
-      resolve();
-    //   console.count(correctAswers);
-    //  console.log(correctAswers);
-    //console.log(correctAswers.length);
-    });
-  });
-}
-
-var output = "";
-
-console.time("concatenation");
-
-for(var p = 1; p<1e6; p++){
-    output +=p;
-}
-
-rl.question("Enter the number of times you want to play: ", async (answer) => {
-  for (let i = 0; i < answer; i++) {
-    var z = Math.floor(getNumberOne(this.x));
-    console.log(z);
-    var s = Math.floor(getNumberTwo(this.y));
-    console.log(s);
-    await askForAnswer(z, s);
-  }
-  rl.close();
-  console.log("You got: ",correctAswers.length, "/", answer, "right");
-  console.timeEnd("concatenation");
-});
 
 
 
