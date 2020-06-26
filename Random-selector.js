4d431fe86df5d3858f944c646f9eec04377f54d5
simtor <simone.ram@ada.ac.uk>
2020-06-26 11:06:55 +0100

    Creating a promise

--- Random-selector.js
+++ Random-selector.js
@@ -6,36 +6,131 @@ const rl = readline.createInterface({
 });
  
 const minimumNumber = 1;
+
+function getNumberOne(min = 1, max = 100) {
+  var x = Math.random() * (max - min) + min;
+  return x;
+}
  
-rl.question(
-  "Enter the maximum number of the randomly generated value: ",
-  (answer) => {
-    var i;
-    for (i = 0; i < answer; i++){
-      // const minimumNumber = 1;
-    function getNumberOne(min = 1, max = 100){
-      var x = Math.random() * (max - min) + min;
-      return (x);
-    // console.log(x);
+function getNumberTwo(min = 1, max = 100) {
+  var y = Math.random() * (max - min) + min;
+  return y;
+}
+
+let correctAswers = [];
+function askForAnswer(z, s) {
+  return new Promise((resolve) => {
+    rl.question("Please input a number answer: ", (input) => {
+      var r = z + s;
+      if (r == input) {
+        console.log("Thats right");
+        correctAswers.push("Right");
+      } else {
+        console.log("Thats wrong");
+      }
+      resolve(input);
+    });
+  });
+}
+ 
+var output = "";
+ 
+console.time("concatenation");
+ 
+for (var p = 1; p < 1e6; p++) {
+  output += p;
+}
+
+  function markResult(mark){
+  if (mark < 0) {
+    console.log('unfortunately you failed better next time. :)');
+  }
+  else if (mark >= 0 && mark <= 49){
+    console.log('unfortunately you failed better next time. :)');
+  }
+  else if (mark >= 50 && mark <= 60) {
+    console.log("Congratulations you have a pass");
+  }
+  else if (mark >= 61 && mark<= 80) {
+    console.log("Congratulations you have a merit");
+  }
+  else if (mark >= 81 && mark<= 100) {
+    console.log("Congratulations you have a Distinction");
+  }
+  else {
+    console.log("Something went wrong with the coded");
+  }
+  return mark;
+} 
++
+
+rl.question("Enter the number of times you want to play: ", async (answer) => {
+  let currentAnswer = null;
+    while (currentAnswer === null || isNaN(currentAnswer)) {
+      currentAnswer = answer;
     }
 
-    function getNumberTwo(min = 1, max = 100){
-      var y = Math.random() * (max - min) + min;
-      return (y);
-    // console.log(x);
+  for (let i = 0; i < answer; i++) {
+    var z = Math.floor(getNumberOne(this.x));
+    var s = Math.floor(getNumberTwo(this.y));
+    if(s>=z){
+      console.log(s, "+", z);
+    }else{
+    console.log(z, "+", s);
     }
-
-    var z = (Math.floor(getNumberOne(this.x)));
-    console.log(z)
-    var s = (Math.floor(getNumberTwo(this.y)));
-    console.log(s)
-
-    var r = (z + s);
-    console.log(r);
-
+    let questionAnswer = null;
+    while (questionAnswer === null || isNaN(questionAnswer)) {
+      questionAnswer = await askForAnswer(z, s);
     }
   }
-);
+
+  rl.close();
+  console.log("You got: ", correctAswers.length, "/", answer, "right");
+  var percentage = (correctAswers.length / answer);
+  var mark = (Math.trunc(percentage*100));
+  console.log(mark, "%");
+   markResult(mark);
+  console.timeEnd("concatenation");
+});
+
+// const readline = require("readline");
+ 
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+ 
+// const minimumNumber = 1;
+ 
+// rl.question(
+//   "Enter the maximum number of the randomly generated value: ",
+//   (answer) => {
+//     var i;
+//     for (i = 0; i < answer; i++){
+//       // const minimumNumber = 1;
+//     function getNumberOne(min = 1, max = 100){
+//       var x = Math.random() * (max - min) + min;
+//       return (x);
+//     // console.log(x);
+//     }
+
+//     function getNumberTwo(min = 1, max = 100){
+//       var y = Math.random() * (max - min) + min;
+//       return (y);
+//     // console.log(x);
+//     }
+
+//     var z = (Math.floor(getNumberOne(this.x)));
+//     console.log(z)
+//     var s = (Math.floor(getNumberTwo(this.y)));
+//     console.log(s)
+
+//     var r = (z + s);
+//     console.log(r);
+
+//     }
+//   }
+// );
 
 // console.log('This is a random game')
 
--- asd.js
+++ asd.js
@@ -1,23 +1,59 @@
-const readline = require("readline");
+// const readline = require("readline");
  
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 
-rl.question("What do you think of node.js? ",(answer)=>{
-    // var max = parseInt(answer, 10);
-    function getNumberOne(min = 1, max=answer) {
-        var x = Math.random() * (max - min) + min;
-        console.log(Math.floor(x))
-      }
-  rl.close();
-});
+// rl.question("what range do you want to play with: ? ",(answer)=>{
+//     // var max = parseInt(answer,  10);
+//     function getNumberOne(min = 1, max=answer) {
+//         var x = Math.random() * (max - min) + min;
+//         console.log(Math.floor(x))
+        
+//       }
+//       let z = Math.floor(getNumberOne(this.x));
+//       let m = getNumberOne.bind(z);
+//       console.log("YOLO");
+//       m();
+      
+//   rl.close();
+// });
+
+// function getNumberTwo(min = 1, max =100
+//   ) {
+//   var y = Math.random() * (max - min) + min;
+//   return y;
+// }
+
+// let z = Math.floor(getNumberTwo(this.y));
+//       let n = getNumberOne.bind(z);
+//       console.log("YOLO");
+//       n();
 
 
 
+function rick(){
+  console.log("Let's get Schwifty!");
+}
+
+let morty = function(){
+  //console.log("I don't think that this is such a good idea Rick.");
+  console.log(this)
+
+};
 
 
-var z = Math.floor(getNumberOne(this.x));
-      console.log(z)
+let summer = f1=>{
+  console.log("About to call another function");
+}
+
+
+// morty();
+
+// rick.call(null);
+// rick.apply();  
+// let o = {};
+// let m = morty.bind(o);
+// m(); 
