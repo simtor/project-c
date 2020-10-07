9d8fa665a426bd0609b84d4648233f53f87d4ab7
simtor <simone.ram@ada.ac.uk>
2020-10-07 09:46:10 +0100

    changed var to let

--- src/game.js
+++ src/game.js
@@ -7,12 +7,12 @@ const rl = readline.createInterface({
 const minimumNumber = 1;
 
 function getNumberOne(min = 1, max = 100) {
-  var x = Math.random() * (max - min) + min;
+  let x = Math.random() * (max - min) + min;
   return x;
 }
  
 function getNumberTwo(min = 1, max = 100) {
-  var y = Math.random() * (max - min) + min;
+  let y = Math.random() * (max - min) + min;
   return y;
 }
 
@@ -20,7 +20,7 @@ let correctAswers = [];
 function askForAnswer(number1, number2) {
   return new Promise((resolve) => {
     rl.question("Please input a number answer: ", (input) => {
-      var r = number1 + number2;
+      let r = number1 + number2;
       if (r == input) {
         console.log("Thats right");
         correctAswers.push("Right");
@@ -32,11 +32,11 @@ function askForAnswer(number1, number2) {
   });
 }
  
-var output = "";
+let output = "";
  
 console.time("concatenation");
  
-for (var p = 1; p < 1e6; p++) {
+for (let p = 1; p < 1e6; p++) {
   output += p;
 }
 
@@ -57,21 +57,16 @@ for (var p = 1; p < 1e6; p++) {
     console.log("Congratulations you have a Distinction");
   }
   else {
-    console.log("Something went wrong with the coded");
+    console.log("Something went wrong with the code");
   }
   return mark;
 }
 
 rl.question("Enter the number of times you want to play: ", async (answer) => {
-  
-  // let currentAnswer = null;
-  //   while (currentAnswer === null || isNaN(currentAnswer)) {
-  //     currentAnswer = await currentAnswer(answer);
-  //   }
 
   for (let i = 0; i < answer; i++) {
-    var number1 = Math.floor(getNumberOne(this.x));
-    var number2 = Math.floor(getNumberTwo(this.y));
+    let number1 = Math.floor(getNumberOne(this.x));
+    let number2 = Math.floor(getNumberTwo(this.y));
     if(number2>=number1){
       console.log(number2, "+", number1);
     }else{
@@ -85,9 +80,32 @@ rl.question("Enter the number of times you want to play: ", async (answer) => {
 
   rl.close();
   console.log("You got: ", correctAswers.length, "/", answer, "right");
-  var percentage = (correctAswers.length / answer);
-  var mark = (Math.trunc(percentage*100));
+  let percentage = (correctAswers.length / answer);
+  let mark = (Math.trunc(percentage*100));
   console.log(mark, "%");
    markResult(mark);
   console.timeEnd("concatenation");
 });
+
+
+
+
+
+// for (let i = 0; i < answer; i++) {
+//   var number1 = Math.floor(getNumberOne(this.x));
+//   var number2 = Math.floor(getNumberTwo(this.y));
+//   if(number2>=number1){
+//     console.log(number2, "+", number1);
+//   }else{
+//   console.log(number1, "+", number2);
+//   }
+//   let questionAnswer = null;
+//   while (questionAnswer === null || isNaN(questionAnswer)) {
+//     questionAnswer = await askForAnswer(number1, number2);
+//   }
+// }
+
+  // let currentAnswer = null;
+  //   while (currentAnswer === null || isNaN(currentAnswer)) {
+  //     currentAnswer = await currentAnswer(answer);
+  //   }
