efa5fca4a2c2d23cfca0616498accf128c5908d3
simtor <simone.ram@ada.ac.uk>
2020-10-07 13:01:52 +0100

    Added right result

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ It's a small game that allows the user to get the brain warm up in the morning.
 
 - The game is based on adding numbers together as fast as possible.
 - The game ask for how question you would like?
-- When the the game is finished you will get the mark in percentage and the time
+- When the the game is finished you will get the mark in percentage and the time.
 
 ## Installation
 **On your terminal console**
@@ -17,4 +17,5 @@ It's a small game that allows the user to get the brain warm up in the morning.
 
 **On your terminal console**
 
-  - `node game.js`
+  - `npm start`
+####  
--- src/game.js
+++ src/game.js
@@ -2,20 +2,15 @@ const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
- 
-const minimumNumber = 1;
-
+}); 
 function getNumberOne(min = 1, max = 100) {
   let x = Math.random() * (max - min) + min;
   return x;
 }
- 
 function getNumberTwo(min = 1, max = 100) {
   let y = Math.random() * (max - min) + min;
   return y;
 }
-
 let correctAswers = [];
 function askForAnswer(number1, number2) {
   return new Promise((resolve) => {
@@ -26,6 +21,7 @@ function askForAnswer(number1, number2) {
         correctAswers.push("Right");
       } else {
         console.log("Thats wrong");
+        console.log("The correct answer was", r);
       }
       resolve(input);
     });
@@ -39,7 +35,6 @@ console.time("concatenation");
 for (let p = 1; p < 1e6; p++) {
   output += p;
 }
-
   function markResult(mark){
   if (mark < 0) {
     console.log('unfortunately you failed better next time. :)');
@@ -63,7 +58,6 @@ for (let p = 1; p < 1e6; p++) {
 }
 
 rl.question("Enter the number of times you want to play: ", async (answer) => {
-
   for (let i = 0; i < answer; i++) {
     let number1 = Math.floor(getNumberOne(this.x));
     let number2 = Math.floor(getNumberTwo(this.y));
@@ -77,7 +71,6 @@ rl.question("Enter the number of times you want to play: ", async (answer) => {
       questionAnswer = await askForAnswer(number1, number2);
     }
   }
-
   rl.close();
   console.log("You got: ", correctAswers.length, "/", answer, "right");
   let percentage = (correctAswers.length / answer);
@@ -86,26 +79,3 @@ rl.question("Enter the number of times you want to play: ", async (answer) => {
    markResult(mark);
   console.timeEnd("concatenation");
 });
-
-
-
-
-
-// for (let i = 0; i < answer; i++) {
-//   var number1 = Math.floor(getNumberOne(this.x));
-//   var number2 = Math.floor(getNumberTwo(this.y));
-//   if(number2>=number1){
-//     console.log(number2, "+", number1);
-//   }else{
-//   console.log(number1, "+", number2);
-//   }
-//   let questionAnswer = null;
-//   while (questionAnswer === null || isNaN(questionAnswer)) {
-//     questionAnswer = await askForAnswer(number1, number2);
-//   }
-// }
-
-  // let currentAnswer = null;
-  //   while (currentAnswer === null || isNaN(currentAnswer)) {
-  //     currentAnswer = await currentAnswer(answer);
-  //   }
